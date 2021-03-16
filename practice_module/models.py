3099859bae1729c7cd976411758237b5f09461ea
Jithin Joseph <jithin.jithin0801@gmail.com>
2021-03-16 08:49:56 +0530

    added slug

--- practice_module/models.py
+++ practice_module/models.py
@@ -3,27 +3,35 @@ from typing import Text
 from django.db import models
 from django.db.models.fields import BooleanField, CharField
 from django.db.models.fields.related import ForeignKey
-
+from django.utils.text import slugify 
 
 # Create your models here.
 
 
 class PracticeMainTopic(models.Model):
-    maintopic = CharField(max_length=50, null=False)
+    title = CharField(max_length=50, null=False)
+    slug = models.SlugField(null=False, blank=True)
 
     def __str__(self):
-        return self.maintopic
-
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(PracticeMainTopic, self).save(*args, **kwargs)
 
 class PracticeSubTopic(models.Model):
     maintopic = models.ForeignKey(
         PracticeMainTopic, on_delete=models.CASCADE)
     title = CharField(max_length=50, null=False)
     content = CharField(max_length=250, null=False)
+    slug = models.SlugField(null=False, blank=True)
 
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(PracticeSubTopic, self).save(*args, **kwargs)
 
 class PracticeProblem(models.Model):
     topic = models.ForeignKey(PracticeSubTopic, on_delete=models.CASCADE)
@@ -39,6 +47,11 @@ class PracticeProblem(models.Model):
     problemtestcasetwooutput = CharField(max_length=200, null=False)
     problemtestcasethreeoutput = CharField(max_length=200, null=False)
     isbookmarked = BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True)
 
     def __str__(self):
         return self.problemtitle
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.problemtitle)
+        super(PracticeProblem, self).save(*args, **kwargs)
--- practice_module/views.py
+++ practice_module/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import PracticeMainTopic, PracticeSubTopic
+from .models import PracticeMainTopic, PracticeProblem, PracticeSubTopic
 # Create your views here.
 
 
@@ -12,5 +12,21 @@ def PracticePage(request):
             "main": main_topic,
             "sub": sub_topic
         }
-        print(PracticeSubTopic.objects.filter(maintopic=1))
     return render(request, "practice_module/practice.html", context)
+
+def ViewChallenges(request, maintopic, subtopic):
+    if request.method == 'GET':
+        topicid = PracticeSubTopic.objects.filter(slug = subtopic).values_list('id', flat=True)
+        maintopicquertset = list(PracticeMainTopic.objects.filter(slug = maintopic).values())
+        subtopicqueryset = list(PracticeSubTopic.objects.filter(slug = subtopic).values())
+        maintitle = maintopicquertset[0]['title']
+        subtitle = subtopicqueryset[0]['title']
+
+        problems = PracticeProblem.objects.filter(topic_id = topicid[0])
+        context = {
+            "title": "CodeRank - Practice",
+            "problems": problems,
+            "maintopic": maintitle,
+            "subtopic": subtitle
+        }
+    return render(request, "practice_module/viewchallenges.html", context)

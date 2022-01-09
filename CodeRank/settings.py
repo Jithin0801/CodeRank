501f414db1396149b38265f09d9a48932cf91b07
Jithin0801 <jithin.jithin0801@gmail.com>
2022-01-09 12:39:24 +0530

    Hide secret key

--- CodeRank/settings.py
+++ CodeRank/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k(a9$rj**-!7u#vju=y^7cv$mo#ydv21y!t_2g6%2ezctx17-x'
+SECRET_KEY = 'Hidden'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
